cbb75a55e14ce4b7ac3657144d98684f6842d217
PERSO <PERSO@192.168.1.27>
2016-10-03 22:04:58 +0200

    Ajout des relations

--- src/main/java/models/Serie.java
+++ src/main/java/models/Serie.java
@@ -1,5 +1,50 @@
 package models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="series")
 public class Serie {
 
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="serie_id")
+	private Long id;
+	
+	
+	@OneToOne
+	@JoinColumn(name="media_id")
+	private Media media_id;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public Media getMedia_id() {
+		return media_id;
+	}
+
+
+	public void setMedia_id(Media media_id) {
+		this.media_id = media_id;
+	}
+
+
+	
 }
