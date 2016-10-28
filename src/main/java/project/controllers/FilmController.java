b96ba98cc2c3f4861f1e6898a30bd65239def430
omaki <omaki@AKIM-ASUS15>
2016-10-28 20:27:18 +0200

    maj ajout film

--- src/main/java/project/controllers/FilmController.java
+++ src/main/java/project/controllers/FilmController.java
@@ -7,19 +7,28 @@ import javax.validation.Valid;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import project.models.Film;
 import project.models.Media;
 import project.repositories.FilmRepository;
 import project.repositories.MediaRepository;
+import project.storage.StorageFileNotFoundException;
 import project.storage.StorageService;
 
 @Controller
@@ -90,17 +99,23 @@ public class FilmController {
 	}
 	
 	@PostMapping("/add")
-	public String add(Model model, @Valid Film film, @Valid Media media, BindingResult bindingResult) {
+	public String add( @Valid Media media, BindingResult bindingResult, @RequestParam("file1") MultipartFile image,
+            RedirectAttributes redirectAttributes) {
 		
 		if(bindingResult.hasErrors())
 		{
 			System.out.println("erreeeeur");
 			return "PAGE_ADD";
 		}
-		Media saveMedia = mediaRep.save(media);
-		System.out.println("NEW SAVED MEDIA WITH ID : "+ saveMedia.getId());
 		
-		film.setMedia(saveMedia);
+		storageService.store(image);
+		
+		media.setImage(image.getOriginalFilename());
+		Media saveMedia = mediaRep.save(media);
+		System.out.println("NEW SAVED MEDIA WITH ID : "+ saveMedia.getId() + " NAME = " + saveMedia.getImage() );
+		
+		Film film = new Film(media);
+		film.setMedia(media);
 		Film saveFilm = filmRep.save(film);
 		
 		System.out.println("NEW SAVED FILM WITH ID : "+ saveFilm.getId());
@@ -113,5 +128,21 @@ public class FilmController {
 		filmRep.delete(id);
 		return "redirect:/" + SUBFOLDER;
 	}
+	
+    @GetMapping("/images/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename()+"\"")
+                .body(file);
+    }
+	
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
 }
 
