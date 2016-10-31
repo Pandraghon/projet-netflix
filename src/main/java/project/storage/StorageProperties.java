fa5737d63904ed5cbf836d0472baccf7f90eb889
PERSO <PERSO@10.78.0.168>
2016-10-31 12:00:50 +0100

    storage

--- src/main/java/project/storage/StorageProperties.java
+++ src/main/java/project/storage/StorageProperties.java
@@ -8,7 +8,7 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private String location = "webflix";
+    private String location = "src/main/resources/static/images/";
 
     public String getLocation() {
         return location;
