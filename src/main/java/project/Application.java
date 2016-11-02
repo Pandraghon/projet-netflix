f05bf36519b4974d972a38722c2e08461dd9b928
Pandraghon <ryuunushi@gmail.com>
2016-11-02 10:50:22 +0100

    Add Category List in Application.java

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -9,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import project.models.Category;
 import project.models.User;
+import project.repositories.CategoryRepository;
 import project.repositories.UserRepository;
 import project.storage.StorageProperties;
 import project.storage.StorageService;
@@ -26,6 +28,9 @@ public class Application implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRep;
+	
+	@Autowired
+	private CategoryRepository categRep;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -53,7 +58,28 @@ public class Application implements CommandLineRunner {
 		user.setRole(false);
 		user.setEmail("akim.neghouche@uha.fr");
 		userRep.save(user);
-		
+                
+                categRep.save(new Category("Action"));
+                categRep.save(new Category("Aventure"));
+                categRep.save(new Category("Animation"));
+                categRep.save(new Category("Biographie"));
+                categRep.save(new Category("Comédie"));
+                categRep.save(new Category("Policier"));
+                categRep.save(new Category("Documentaire"));
+                categRep.save(new Category("Drame"));
+                categRep.save(new Category("Famille"));
+                categRep.save(new Category("Fantaisie"));
+                categRep.save(new Category("Film-Noir"));
+                categRep.save(new Category("Historique"));
+                categRep.save(new Category("Horreur"));
+                categRep.save(new Category("Musique"));
+                categRep.save(new Category("Mystère"));
+                categRep.save(new Category("Romance"));
+                categRep.save(new Category("Sci-Fi"));
+                categRep.save(new Category("Sport"));
+                categRep.save(new Category("Thriller"));
+                categRep.save(new Category("Guerre"));
+                categRep.save(new Category("Western"));
 	}
 
 
